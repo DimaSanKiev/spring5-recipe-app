0415d74500c2261354b71cce2cfb665457e4560f
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-09-01 23:42:48 +0200

    Refactor structure

--- src/main/java/guru/springframework/recipe/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/recipe/Spring5RecipeAppApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5recipeapp;
+package guru.springframework.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/guru/springframework/recipe/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/recipe/Spring5RecipeAppApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5recipeapp;
+package guru.springframework.recipe;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
