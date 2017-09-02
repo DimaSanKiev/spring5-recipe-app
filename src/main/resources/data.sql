57df15f4608bd41c7b526a2004228ca8c46d1472
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-09-02 12:31:06 +0200

    Add data.sql Spring Data initializer script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO category (category_name) VALUES ('Ukrainian');
+INSERT INTO category (category_name) VALUES ('Scandinavian');
+INSERT INTO category (category_name) VALUES ('American');
+INSERT INTO category (category_name) VALUES ('French');
+INSERT INTO category (category_name) VALUES ('Australian');
+INSERT INTO category (category_name) VALUES ('Italian');
+INSERT INTO category (category_name) VALUES ('Mexican');
+INSERT INTO category (category_name) VALUES ('Fast Food');
+
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
+INSERT INTO unit_of_measure (description) VALUES ('Grams');
