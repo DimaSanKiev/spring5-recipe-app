95ee85d45aa47bfa0fef21f6f9c60cfce734521b
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-09-03 01:00:54 +0200

    Refactor using Lombok

--- src/main/java/guru/springframework/recipe/service/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/recipe/service/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package guru.springframework.recipe.service;
 
 import guru.springframework.recipe.model.Recipe;
 import guru.springframework.recipe.repostitory.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
