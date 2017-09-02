c12654e1f32936b32453673dd47e2fe4442cdbfd
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-09-02 12:48:34 +0200

    Add Spring Data JPA query methods

--- src/main/java/guru/springframework/recipe/controller/IndexController.java
+++ src/main/java/guru/springframework/recipe/controller/IndexController.java
@@ -1,13 +1,34 @@
 package guru.springframework.recipe.controller;
 
+import guru.springframework.recipe.model.Category;
+import guru.springframework.recipe.model.UnitOfMeasure;
+import guru.springframework.recipe.repostitory.CategoryRepository;
+import guru.springframework.recipe.repostitory.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByCategoryName("Ukrainian");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID is: " + categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure ID is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/guru/springframework/recipe/repostitory/CategoryRepository.java
+++ src/main/java/guru/springframework/recipe/repostitory/CategoryRepository.java
@@ -3,5 +3,9 @@ package guru.springframework.recipe.repostitory;
 import guru.springframework.recipe.model.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByCategoryName(String categoryName);
 }
