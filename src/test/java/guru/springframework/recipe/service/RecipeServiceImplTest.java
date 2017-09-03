90c4cd35b9fd8f18e6a5e2f84dc819117fecc8ec
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-09-03 17:47:23 +0200

    Add RecipeService test with Mockito mock

---
+++ src/test/java/guru/springframework/recipe/service/RecipeServiceImplTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.recipe.service;
+
+import guru.springframework.recipe.model.Recipe;
+import guru.springframework.recipe.repostitory.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    private RecipeService recipeService;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+}
